18360f697d0400ff85d913c1df98299067d60114
Samuel Williams <samueltwilliams@gmail.com>
2013-10-14 20:33:11 +0100

    moved to clojurescript
    added a first pass canvas rendering

--- project.clj
+++ project.clj
@@ -2,4 +2,17 @@
   :description "redoing my degree - in a browser, sorta"
   :license {:name "MIT"
             :url "http://sammyt.mit-license.org"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :repositories {"sonatype-staging" "https://oss.sonatype.org/content/groups/staging"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1913"]
+                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
+                 [com.keminglabs/c2 "0.2.3"]]
+  :hooks [leiningen.cljsbuild]
+  :plugins [[lein-cljsbuild "0.3.3"]]
+  :cljsbuild {
+     :builds [{
+       :id "dev"
+       :source-paths ["src/retro"]
+       :compiler {
+          :output-to "public/retro-dev.js"
+          :pretty-print true }} ]})
---
+++ src/retro/core.cljs
@@ -0,0 +1,127 @@
+(ns retro.core
+  (:require
+    [c2.core]
+    [cljs.core.async :refer [>! <! chan timeout alts!]])
+  (:require-macros 
+    [c2.util :refer [bind! pp]]
+    [cljs.core.async.macros :refer [go]]))
+
+(defn- exp 
+  ([pow] (Math/exp pow)))
+
+(defn- row 
+  "helper function to generate vector of random 1s and -1s"
+  [i] 
+  (vec (take i (repeatedly 
+                 #(let [i (rand-int 2)] (if (zero? i) -1 i))))))
+
+(defn- flip
+  [spin]
+  (* -1 spin))
+
+(defn flip!
+  [lattice [i j]]
+  (let [row (nth lattice j)]
+    (assoc lattice j
+      (assoc row i (flip (nth row i))))))
+
+(defn make-lattice 
+  "build a lattice of dimension i by j"
+  [i j] 
+  (vec (take j (repeatedly #(row i)))))
+
+(defn rand-pos
+  "generate a random position within a lattice"
+  [lattice]
+  (let [j (int (count lattice))
+        i (int (count (first lattice)))]
+    [(rand-int i) (rand-int j)]))
+
+(defn spin-at
+  "find the spin at a given lattice position"
+  [lattice [i j]]
+  (nth (nth lattice j) i))
+
+(defn neighbours-spins 
+  "find the spins of the nearest neighbours"
+  [lattice [i j]]
+  (let [h (count lattice)
+        w (count (first lattice))]
+    (map 
+      (partial spin-at lattice) 
+      [[i (mod (inc j) h)]
+       [i (mod (dec j) h)]
+       [(mod (inc i) w) j]
+       [(mod (dec i) w) j]])))
+
+(defn energy
+  [spin nn-spins]
+  (* 2 spin (reduce + nn-spins)))
+
+(defn total-energy 
+  [lattice]
+  (reduce + (flatten lattice)))
+
+(defn metropolis-step
+  [lattice temp]
+  (let [pos  (rand-pos lattice)
+        spin (spin-at lattice pos)
+        neighbours (neighbours-spins lattice pos)
+        before (energy spin neighbours)
+        after  (energy (flip spin) neighbours)
+        delta (- before after)]
+    (if (or (<= delta 0) (< (rand) (exp (- (/ delta temp)))))
+      (flip! lattice pos)
+      lattice)))
+
+(defn simulate-at-temp
+  ([lattice temp] 
+    (simulate-at-temp lattice temp (count lattice)))
+  ([lattice temp iterations]
+    (loop [lattice lattice i 0]
+      (if (< i iterations)
+        (recur 
+          (metropolis-step lattice temp)
+          (inc i))
+        lattice))))
+
+
+(bind! 
+  "div.ising" 
+  [:div [:canvas.draw {:height (str 500 "px")
+                       :width (str 500 "px")}]])
+
+(defn draw-rect
+  [ctx [x y] [h w]]
+  (.fillRect ctx x y h w))
+
+(defn draw-spin
+  [ctx size spin pos]
+  (doall 
+    (if (neg? spin)
+      (aset ctx "fillStyle" "#ff0000")
+      (aset ctx "fillStyle" "#00ff00"))
+    (draw-rect ctx pos size)))
+
+(defn draw-lattice
+  [lattice draw]
+  (map-indexed 
+    (fn [j r] 
+      (doall 
+        (map-indexed
+          (fn [i s]
+            (draw s [(* j 21) (* i 21)] [20 20]))
+          r)))
+    lattice))
+
+(go
+  (let [canvas (dom/getElementByClass "draw")
+        ctx (.getContext canvas "2d") ]
+    (loop [lattice (make-lattice 30 30)]
+      (<! (timeout 5))
+      (doall 
+        (draw-lattice
+          lattice
+          (partial draw-spin ctx [20 20])))
+      (recur (simulate-at-temp lattice 1 100)))))
+
